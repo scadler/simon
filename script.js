404c82aaa777df686a68a8e6255a96b96cb37077
scadler <scadler@s.sfusd.edu>
2020-05-29 19:13:33 +0000

    completed mvp of game, will now work on improving

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const status = {
     level: 2,
     sequence: [],
+    input: [],
+    entering: false,
+    pause: false,
+    correctInput: true,
 }
 function createPattern(level){
-    status.sequence = []
+    status.entering = false;
+    status.sequence = [];
+    status.input = [];
+    status.correctInput = true
     var i = 0
     while( i <= level){
         var order = Math.floor(Math.random()*4)
@@ -13,16 +20,56 @@ function createPattern(level){
     }
     console.log(status.sequence)
     status.level += 1
-    // showPattern(status.sequence[0], 0)
+    showPattern(status.sequence[0], 0)
 }
 function showPattern(color, i){
-    $("#"+color).css("opacity","0.6")
-    while(i <status.sequence.length){
+    $("#"+status.sequence[i-1]).css("opacity","0.6")
+    if(i <status.sequence.length){
         i++
         $("#"+color).css("opacity","1")
-        setTimeout(showPattern, 333, status.sequence[i], i)
+        setTimeout(clearTiles, 600, status.sequence[i], i)
+    }
+    else if(i === status.sequence.length){
+        status.entering = true;
+    }
+}
+function clearTiles(nextColor, index){
+    $(".tile").css("opacity","0.6")
+    if(status.entering === false){
+        setTimeout(showPattern, 300, nextColor, index)
+    }
+    else{
+        status.pause = true
+        setTimeout(function(){status.pause = false}, 300)
+    }
+}
+function checkInput(){
+    if(status.input.length === status.sequence.length){
+        var i = 0;
+        while(i < status.input.length){
+            if(status.input[i] !== status.sequence[i]){
+                status.correctInput = false
+            }
+            console.log(i)
+            console.log(status.input[i])
+            i++
+        }
+        if(status.correctInput === true){
+            console.log("Correct Input")
+            setTimeout(createPattern, 1000, status.level)
+        }
     }
 }
 $("#start").click(function(){
     createPattern(status.level);
+});
+$(".tile").click(function(){
+    var id = $(this).attr('id')
+    if(status.entering === true && status.pause === false){
+        $("#"+id).css("opacity","1")
+        status.input.push(id)
+        status.pause = true
+        setTimeout(clearTiles, 600)
+        checkInput()
+    }
 });
